04a54c4a483a6233506aeb30ba3b9b7d8b3e5946
Knut Ahlers <knut@ahlers.me>
2017-07-20 08:56:55 +0200

    Use only first part of hostname
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- bin/color_from_hostname.py
+++ bin/color_from_hostname.py
@@ -2,11 +2,11 @@
 
 import socket
 
-hostname = socket.gethostname()
+hostname = socket.gethostname().split('.')[0]
 
 s = 0
 for c in hostname:
-  s = s + ord(c)
+    s = s + ord(c)
 
 color = str(1 + s % 14).rjust(3, '0')
 print '$FG[{}]'.format(color)
