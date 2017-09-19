eeb4ba2d79bc52f6dc4dd512a340a2dd2da73cb0
Knut Ahlers <knut@ahlers.me>
2017-09-19 12:10:53 +0200

    Unify scripts by using a common color / function framework
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

---
+++ bin/script_framework.sh
@@ -0,0 +1,20 @@
+COLOR_RED="\033[0;31m"
+COLOR_GREEN="\033[0;32m"
+COLOR_CYAN="\033[0;36m"
+COLOR_PLAIN="\033[0m"
+
+function error {
+  echo -e "${COLOR_RED}$@${COLOR_PLAIN}"
+}
+
+function success {
+  echo -e "${COLOR_GREEN}$@${COLOR_PLAIN}"
+}
+
+function info {
+  echo -e "${COLOR_CYAN}$@${COLOR_PLAIN}"
+}
+
+function step {
+    info "[$(date +%H:%M:%S)] $@"
+}
