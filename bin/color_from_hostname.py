518177dd01211db1f8429a1c6b78e4f434ec0d7c
Knut Ahlers <knut@ahlers.me>
2017-12-14 11:18:00 +0100

    Ensure py2/3 compatibiliy

--- bin/color_from_hostname.py
+++ bin/color_from_hostname.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import socket
 
 hostname = socket.gethostname().split('.')[0]
@@ -9,4 +11,4 @@ for c in hostname:
     s = s + ord(c)
 
 color = str(1 + s % 14).rjust(3, '0')
-print '$FG[{}]'.format(color)
+print('$FG[{}]'.format(color))
