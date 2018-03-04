5aeec941373c350b35945723a7410044ada9cbb1
Knut Ahlers <knut@ahlers.me>
2018-03-04 23:09:10 +0100

    Unify all "config" git wrapper under one
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- .zsh/config-git.zsh
+++ .zsh/config-git.zsh
@@ -1,7 +1,12 @@
 function config {
-  git --git-dir=${HOME}/.cfg/public --work-tree=${HOME} $@
+  repo_name=$1
+  shift
+
+  if ! [ -d ${HOME}/.cfg/${repo_name} ]; then
+    echo "Repo '${repo_name}' not found!"
+    return 1
+  fi
+
+  git --git-dir=${HOME}/.cfg/${repo_name} --work-tree=${HOME} $@
 }
 
-function config_secret {
-  git --git-dir=${HOME}/.cfg/secret --work-tree=${HOME} $@
-}
