29f554915e001c60603e241f88f1cdbbc06401f1
Knut Ahlers <knut@ahlers.me>
2018-10-07 14:41:29 +0200

    Logging should go to stderr
    Also added "fatal" alias to fail and applied shfmt
    
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- bin/script_framework.sh
+++ bin/script_framework.sh
@@ -5,26 +5,30 @@ COLOR_YELLOW="\033[0;33m"
 COLOR_PLAIN="\033[0m"
 
 function error() {
-  echo -e "${COLOR_RED}$@${COLOR_PLAIN}"
+	echo -e "${COLOR_RED}$@${COLOR_PLAIN}" >&2
 }
 
 function fail() {
-  error "$@"
-  exit 1
+	error "$@"
+	exit 1
+}
+
+function fatal() {
+	fail "$@"
 }
 
 function info() {
-  echo -e "${COLOR_CYAN}$@${COLOR_PLAIN}"
+	echo -e "${COLOR_CYAN}$@${COLOR_PLAIN}" >&2
 }
 
 function step() {
-  info "[$(date +%H:%M:%S)] $@"
+	info "[$(date +%H:%M:%S)] $@"
 }
 
 function success() {
-  echo -e "${COLOR_GREEN}$@${COLOR_PLAIN}"
+	echo -e "${COLOR_GREEN}$@${COLOR_PLAIN}" >&2
 }
 
 function warn() {
-  echo -e "${COLOR_YELLOW}$@${COLOR_PLAIN}"
+	echo -e "${COLOR_YELLOW}$@${COLOR_PLAIN}" >&2
 }
