43bd67f8641f2258dfc8681ac19a0a4a52651d66
Knut Ahlers <knut@ahlers.me>
2021-04-14 01:49:23 +0200

    Fix comment, add more directives
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,7 @@ module.exports = {
 
   extends: [
     'plugin:vue/strongly-recommended',
-    'eslint:recommended', // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'eslint:recommended', // https://eslint.org/docs/rules/
   ],
 
   globals: {
@@ -52,6 +52,8 @@ module.exports = {
     'vue',
   ],
 
+  reportUnusedDisableDirectives: true,
+
   root: true,
 
   rules: {
@@ -62,10 +64,13 @@ module.exports = {
     'arrow-spacing': ['error', { after: true, before: true }],
     'block-spacing': ['error'],
     'brace-style': ['error', '1tbs'],
+    'camelcase': ['error'],
     'comma-dangle': ['error', 'always-multiline'],
     'comma-spacing': ['error'],
     'comma-style': ['error', 'last'],
     'curly': ['error'],
+    'default-case-last': ['error'],
+    'default-param-last': ['error'],
     'dot-location': ['error', 'property'],
     'dot-notation': ['error'],
     'eol-last': ['error', 'always'],
@@ -81,19 +86,28 @@ module.exports = {
     'lines-between-class-members': ['error'],
     'multiline-comment-style': ['warn'],
     'newline-per-chained-call': ['error'],
+    'no-alert': ['error'],
     'no-console': ['off'],
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off', // allow debugger during development
     'no-duplicate-imports': ['error'],
     'no-else-return': ['error'],
+    'no-empty-function': ['error'],
     'no-extra-parens': ['error'],
     'no-implicit-coercion': ['error'],
     'no-lonely-if': ['error'],
     'no-multi-spaces': ['error'],
     'no-multiple-empty-lines': ['warn', { max: 2, maxBOF: 0, maxEOF: 0 }],
+    'no-promise-executor-return': ['error'],
+    'no-return-assign': ['error'],
+    'no-script-url': ['error'],
+    'no-template-curly-in-string': ['error'],
     'no-trailing-spaces': ['error'],
     'no-unneeded-ternary': ['error'],
+    'no-unreachable-loop': ['error'],
+    'no-unsafe-optional-chaining': ['error'],
     'no-useless-return': ['error'],
     'no-var': ['error'],
+    'no-warning-comments': ['error'],
     'no-whitespace-before-property': ['error'],
     'object-curly-newline': ['error', { consistent: true }],
     'object-curly-spacing': ['error', 'always'],
@@ -106,6 +120,8 @@ module.exports = {
     'prefer-template': ['error'],
     'quote-props': ['error', 'consistent-as-needed', { keywords: false }],
     'quotes': ['error', 'single', { allowTemplateLiterals: true }],
+    'require-atomic-updates': ['error'],
+    'require-await': ['error'],
     'semi': ['error', 'never'],
     'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: false, ignoreMemberSort: false }],
     'sort-keys': ['error', 'asc', { caseSensitive: true, natural: false }],
