498e139266822194fc063a0c4c380c9fb127219c
Knut Ahlers <knut@ahlers.me>
2021-12-13 18:57:02 +0100

    Rework script_framework, add log-levels
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- bin/script_framework.sh
+++ bin/script_framework.sh
@@ -6,15 +6,16 @@ COLOR_PLAIN="\033[0m"
 COLOR_PURPLE="\033[35m"
 
 function check_util() {
-	which ${1} >/dev/null 2>&1 || fail "Missing ${1} util"
+	command -v ${1} >/dev/null 2>&1 || fail "Missing ${1} util"
 }
 
 function debug() {
-	[[ ${DEBUG:-false} != false ]] || return 0
+	log_level_matches 0 || return 0
 	echo -e "${COLOR_PURPLE}$@${COLOR_PLAIN}"
 }
 
 function error() {
+	log_level_matches 3 || return 0
 	echo -e "${COLOR_RED}$@${COLOR_PLAIN}" >&2
 }
 
@@ -28,7 +29,19 @@ function fatal() {
 }
 
 function info() {
+	log_level_matches 1 || return 0
 	echo -e "${COLOR_CYAN}$@${COLOR_PLAIN}" >&2
+}
+
+function log_level_matches() {
+	declare -A log_levels=(
+		[debug]=0
+		[info]=1
+		[warn]=2
+		[warning]=2
+		[error]=3
+	)
+	[ ${log_levels[${LOG_LEVEL:-UNDEF}]:-1} -le ${1} ] && return 0 || return 1
 }
 
 function step() {
@@ -36,9 +49,11 @@ function step() {
 }
 
 function success() {
+	log_level_matches 1 || return 0
 	echo -e "${COLOR_GREEN}$@${COLOR_PLAIN}" >&2
 }
 
 function warn() {
+	log_level_matches 2 || return 0
 	echo -e "${COLOR_YELLOW}$@${COLOR_PLAIN}" >&2
 }
